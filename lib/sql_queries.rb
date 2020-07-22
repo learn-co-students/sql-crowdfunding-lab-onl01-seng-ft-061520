d193c965b6e1c00ffb0ece00f6aeda4ed6a4b9ce
Amonsta <amana.ammi@gmail.com>
2020-07-22 15:44:51 -0600

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,6 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  
   "SELECT projects.title, SUM(pledges.amount) FROM projects 
   INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title;"
 end
@@ -23,10 +22,14 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "Write your SQL query Here"
+  # SELECT users.name, SUM(pledges.amount), 
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "Write your SQL query Here"
+  "SELECT projects.category, pledges.amount FROM projects INNER JOIN pledges 
+  ON projects.id = pledges.project_id
+  WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
